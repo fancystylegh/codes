a6bc67f8062bf0276bbbd0500022c2b491c551ba
fancystylegh <fancystylegh@163.com>
2017-02-07 11:12:46 +0800

    响应式网站

---
+++ ResponsiveWeb/gulpfile.js
@@ -0,0 +1,32 @@
+/**
+ * Created by gdc on 2017/2/3.
+ */
+var gulp = require('gulp');
+gulp.task('task-name',function(){
+    var gulp = require('gulp');
+    var rev = require('gulp-rev');
+    var revReplace = require('gulp-rev-replace');
+    var useref = require('gulp-useref');
+    var filter = require('gulp-filter');
+    var uglify = require('gulp-uglify');
+    var csso = require('gulp-csso');
+
+    gulp.task('default',function(){
+        var jsFilter =filter('**/*.js',{restore:true});
+        var cssFilter = filter('**/*.css',{restore:true});
+        var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
+        return gulp.src('src/index.html')
+            .pipe(useref())
+            .pipe(jsFilter)
+            .pipe(uglify())
+            .pipe(jsFilter.restore)
+            .pipe(cssFilter)
+            .pipe(csso())
+            .pipe(cssFilter.restore)
+            .pipe(indexHtmlFilter)
+            .pipe(rev())
+            .pipe(indexHtmlFilter.restore)
+            .pipe(revReplace())
+            .pipe(gulp.dest('dest'));
+    });
+});
---
+++ ResponsiveWeb/src/javascript/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gdc on 2017/2/3.
+ */
+  /* 调用oc*/
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoPlay: true,
+        autoPlayTimeout: 3000,
+        autoPlayHoverPause: true
+    });
+});
